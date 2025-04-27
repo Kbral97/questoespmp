fe7f7051112f13b916b6233023c0e52d9a6ae09c
Kbral97 <gabrielcabralgomes@hotmail.com>
2025-04-27 13:25:49 -0300

    fix: configuração do CSRF protection

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 from .config import Config
 from dotenv import load_dotenv
 
@@ -27,6 +28,7 @@ else:
 db = SQLAlchemy()
 login_manager = LoginManager()
 migrate = Migrate()
+csrf = CSRFProtect()
 
 # Configuração do Flask-Login
 login_manager.login_view = 'main.login'
@@ -66,6 +68,7 @@ def create_app(config_class=Config):
     db.init_app(app)
     login_manager.init_app(app)
     migrate.init_app(app, db)
+    csrf.init_app(app)
     
     # Registro dos blueprints
     from app.routes import main as main_blueprint
